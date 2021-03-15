06bbcc16af254e6d491e26baf6428fa12c8689e9
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2021-03-14 21:58:09 -0300

    added new images to login page

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="container">
-    <LoginForm />
+  <div id="container">
+    <div id="login-form">
+      <LoginForm />
+    </div>
+
+    <div id="background"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import LoginForm from "@/components/login/LoginForm.vue"
 
 export default {
@@ -15,3 +18,29 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+#container {
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  height: 100%;
+  min-height: 450px;
+  width: 100%;
+}
+
+#login-form {
+  width: 50vw;
+}
+#background {
+  background-image: url("../assets/login-background.svg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  width: 50vw;
+  height: 100vh;
+}
+</style>
