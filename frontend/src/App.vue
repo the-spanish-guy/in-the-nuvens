3574bfb1452acf3719598a5a3e53a1291842270f
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2021-03-28 18:41:59 -0300

    feat (frontend) set new colorschemes create default colorschemes and import on styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <v-app v-dark-mode>
     <router-view />
   </v-app>
 </template>
@@ -11,11 +11,10 @@ export default {
 </script>
 
 <style scoped>
-
 @import url("https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons");
 @import url("https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css");
 
-@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&family=Roboto:wght@400;700&display=swap');
-
+@import url("https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&family=Roboto:wght@400;700&display=swap");
+@import url(./styles/root.css);
 @import url(./styles/global.css);
 </style>
--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -7,22 +7,22 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
-        primary: '#8C7CFC',
-        secondary: '#85AFF9',
+        primary: '#FFFFFF',
+        secondary: '#54C4E4',
+        terciary: '#85AFF9',
         accent: '#54C4E4',
-        error: '#b71c1c',
-        text: {
-          primary: '#FF00FF'
-        }
+        error: '#F98585',
+        warning: '#FFE27C',
+        success: '#A3DE9A'
       },
       dark: {
         primary: '#1B2135',
-        secondary: '#b0bec5',
-        accent: '#8c9eff',
-        error: '#b71c1c',
-        text: {
-          primary: '#0000FF'
-        }
+        secondary: '#54C4E4',
+        terciary: '#85AFF9',
+        accent: '#54C4E4',
+        error: '#F98585',
+        warning: '#FFE27C',
+        success: '#A3DE9A'
       }
     }
   }
