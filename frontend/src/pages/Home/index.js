f7fb39cbe9ab1378ec1962685a615e9a24f04860
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2021-08-02 23:39:07 -0300

    :sparkles: feat: add home page

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,5 +1,24 @@
-import React from "react";
+import { MdHome } from 'react-icons/md'
+
+import logo from "../../assets/logo.png";
 
 export default function Home() {
-  return <h1>Hola Mundo</h1>;
+
+  return (
+    <div className="w-screen h-screen dark:bg-background-dark flex">
+      <div className="w-56 h-screen py-9 px-6">
+        <img className="w-48 h-11" src={logo} alt="logo" />
+        <div className="items">
+          <div className="item">
+            <MdHome size="24" color="text-red" />
+            <span>Meus Arquivos</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="h-screen py-9 px-6 bg-pink-700 flex-1">
+        <h1>Meus Arquivos</h1>
+      </div>
+    </div>
+  );
 }
