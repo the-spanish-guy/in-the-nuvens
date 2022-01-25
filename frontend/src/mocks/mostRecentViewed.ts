3267fd169566eca5f53d0707af7e0880f5eb56ff
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2022-01-25 14:33:25 -0300

    :clown_face: feat: added mocks

---
+++ frontend/src/mocks/mostRecentViewed.ts
@@ -0,0 +1,13 @@
+export const files = [{
+  isFolder: false,
+  nome: 'teste',
+  length: '19 arquivos'
+}]
+export const folders = [{
+  isFolder: false,
+  nome: 'teste',
+  length: '19 arquivos'}]
+export const mostRecent = [{
+  isFolder: false,
+  nome: 'teste',
+  length: '19 arquivos'}]
