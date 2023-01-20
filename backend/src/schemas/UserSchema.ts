1bd8adc60257ab0102de6837130487f4b6357f0a
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2023-01-19 21:11:15 -0300

    :pencil2: improve typos

--- backend/src/schemas/UserSchema.ts
+++ backend/src/schemas/UserSchema.ts
@@ -2,17 +2,13 @@ import { model, Schema } from 'mongoose'
 import * as bcrypt from 'bcrypt'
 
 export interface IUser {
-  name: string
-  login: string
-  email: string
+  username: string
   password: string
 }
 
-const UserSchema: Schema = new Schema(
+const UserSchema: Schema = new Schema<IUser>(
   {
-    name: { type: String, required: true, trim: true },
-    email: { type: String, required: true, trim: true },
-    login: { type: String, required: true, trim: true },
+    username: { type: String, required: true, trim: true },
     password: { type: String, required: true, trim: true }
   },
   {
